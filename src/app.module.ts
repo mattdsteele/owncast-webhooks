5620f000d244480e3f6149a86b529a7687fa316c
mattdsteele <orphum@gmail.com>
2021-10-11 20:38:03 -0500

    Update modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AnnouncerToken } from './announcers/announcer.service';
+import { CompositeAnnouncer } from './announcers/composite-announcers.service';
+import { DiscordService } from './announcers/discord.service';
+import { MastodonService } from './announcers/mastodon.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CompositeAnnouncer } from './composite-announcers.service';
 import { OwncastWebhookController } from './owncast-webhook/owncast-webhook.controller';
 
 @Module({
   imports: [],
   controllers: [AppController, OwncastWebhookController],
-  providers: [AppService, { provide: AnnouncerToken, useClass: CompositeAnnouncer}],
+  providers: [
+    AppService,
+    { provide: AnnouncerToken, useClass: CompositeAnnouncer },
+    DiscordService,
+    MastodonService,
+  ],
 })
 export class AppModule {}
