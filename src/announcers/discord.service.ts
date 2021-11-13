b9616fa732b14c58bc1416ca167cbefe30c848a2
Matt Steele <orphum@gmail.com>
2021-11-13 08:44:24 -0600

    feat: support multiple discord channels

--- src/announcers/discord.service.ts
+++ src/announcers/discord.service.ts
@@ -3,6 +3,8 @@ import { Announcer } from './announcer.service';
 import Discord = require('discord.js');
 const { DISCORD_BOT_TOKEN, DISCORD_CHANNEL, OWNCAST_SERVER_URL } = process.env;
 
+const channelList: string[] = DISCORD_CHANNEL.split(',');
+
 @Injectable()
 export class DiscordService implements Announcer {
   client: Discord.Client;
@@ -15,12 +17,14 @@ export class DiscordService implements Announcer {
       this.client = new Discord.Client();
       const loggedIn = await this.client.login(DISCORD_BOT_TOKEN);
       console.log('logged in', loggedIn);
+      console.log('searching for channels', DISCORD_CHANNEL, channelList);
       console.log(this.client.channels.cache.size);
       const channel = this.client.channels.cache.find(
-        (x: any) => x.name === DISCORD_CHANNEL,
+        (x: any) => channelList.includes(x.name)
       );
       console.log(channel);
       if (channel && channel.isText()) {
+        console.log('found and configured discord channel for', channel.name);
         this.channel = channel as Discord.TextChannel;
         return true;
       }
